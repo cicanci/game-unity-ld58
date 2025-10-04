c6100e611359cc3f23606130b0bb7174b8d8f982
Bruno Cicanci <brunocicanci@gmail.com>
2025-10-04 14:28:18 +0100

    Saving the 2D tilemap test

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // new Input System
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private PlayerControls controls;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+    void OnEnable()
+    {
+        controls.Player.Enable();
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
